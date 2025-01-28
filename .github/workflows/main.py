674d184029473fab91fe479a7fd3519f719755e2
Hishmath Deen <HishmathD@fortude.co>
2025-01-28 23:47:34 +0530

    Action yml update

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -5,70 +5,50 @@ owner = 'hishmathdeen'
 repo = 'demo-terra-atlantis'
 pr_number = os.getenv('PR_NUMBER')
 token = os.getenv('GT_TOKEN')
+dir_path = os.getenv('DIR')
 
 file_path = '/home/runner/work/terragrunt-testing/terragrunt-testing/plans/plan.txt'
 with open(file_path, 'r') as file:
     file_data = file.read()
 
 comment = f"""
-### :memo: PR Summary
-
-**Summary**: This PR improves the performance of the system by refactoring the data processing module.
-
----
-
-### :rocket: Detailed View
-
-**Changes made:**
-- Refactored data processing logic in `data_processor.py`.
-- Optimized the database queries for better performance.
-- Added unit tests for the new functions.
-
-**Performance improvements**:
-- Data processing time reduced by 20%.
-- CPU usage during peak times dropped by 15%.
+### :rocket: Infrasturece Change requtested
+:memo: Terragrant File updated in `{dir_path}`
 
 ---
 
-:checkered_flag: Ready for review!
-
----
-
-### :bulb: Notes:
-Please test on the staging environment before merging. :warning:
-
----
+### :bulb: View the Plan
 
 <details>
-  <summary>Click to see the detailed data</summary>
+  <summary>Terragrunt Plan</summary>
 
   Here is the data from the text file:
 
   ```txt
   {file_data}
   ```
-</details> """
+</details>
 
+---
 
-# GitHub API URL to post a comment on a PR
+:checkered_flag: Review and Merge the PR
+
+"""
+
 url = f'https://api.github.com/repos/{owner}/{repo}/issues/{pr_number}/comments'
 
-# HTTP headers
 headers = {
     'Authorization': f'token {token}',
     'Accept': 'application/vnd.github.v3+json'
 }
 
-# Payload with the comment body
 payload = {
     'body': comment
 }
 
-# Send POST request to create the comment
 response = requests.post(url, headers=headers, json=payload)
 
-# Check the response
 if response.status_code == 201:
-    print('Comment posted successfully.')
+    print('Comment posted')
 else:
     print(f'Failed to post comment: {response.status_code} - {response.text}')
